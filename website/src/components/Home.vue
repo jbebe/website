8c6395099d962ea51a8526a73690e396f6d9f25b
Bálint Juhász <juhasz.balint.bebe@gmail.com>
2021-05-16 19:39:45 +0200

    add design, create menu

---
+++ website/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Header />
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
