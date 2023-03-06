9a07b441a530972a4882ad04d0afcf1bcfcf8e46
Bálint Juhász <juhasz.balint.bebe@gmail.com>
2023-03-06 23:08:51 +0100

    workaround for missing css in astro/mdx/astro component

--- src/utils/collection-helpers.ts
+++ src/utils/collection-helpers.ts
@@ -3,6 +3,7 @@ import type { CollectionTypes } from '@utils/types';
 
 export async function getPosts<T extends CollectionTypes>(collection: T) {
   const posts = (await getCollection(collection)) as unknown as CollectionEntry<'articles'>[];
+  console.log(posts);
   return posts
     .filter((x) => x.data.live)
     .map((post) => ({
