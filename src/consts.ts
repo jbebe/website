4b7fdba610f20910159933471149028a19189583
Bálint Juhász <juhasz.balint.bebe@gmail.com>
2023-03-07 22:44:56 +0100

    start new article [skip ci]

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,10 @@
 export const META_TITLE = 'balint.cloud';
 export const SITE_TITLE = "Bálint Juhász's personal home page";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
 export enum Tag {
   Web = 'web',
   Exotic = 'exotic',
+  Cpp = 'c++',
+  Networking = 'networking',
 }
