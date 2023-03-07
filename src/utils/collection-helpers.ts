1cb006ea16b1752abd55afb20ed910d30c33610c
Bálint Juhász <juhasz.balint.bebe@gmail.com>
2023-03-07 09:34:00 +0100

    finish article

--- src/utils/collection-helpers.ts
+++ src/utils/collection-helpers.ts
@@ -4,7 +4,7 @@ import type { CollectionTypes } from '@utils/types';
 export async function getPosts<T extends CollectionTypes>(collection: T) {
   const posts = (await getCollection(collection)) as unknown as CollectionEntry<'articles'>[];
   return posts
-    .filter((x) => x.data.live)
+    .filter((x) => import.meta.env.DEV || x.data.live)
     .map((post) => ({
       params: { slug: post.slug },
       props: post,
